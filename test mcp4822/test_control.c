9dc48db5b7dfa49e9e927c314e49066ee7302565
meerc <76517847+meerc@users.noreply.github.com>
2022-11-03 11:42:58 +0100

    Update test_control.c

--- test mcp4822/test_control.c
+++ test mcp4822/test_control.c
@@ -10,19 +10,22 @@
 int main()
 {
     bool chk=true;
+    int i=0;
     uint16_t i=1000u;
 
     mcp4822_initialize();
 
 
-    while(chk== true&& i<4095u)
+    while(chk== true&& i<4095u&& i<10000000)
     {
 
     chk=mcp4822_set_voltage(MCP_4822_CHANNEL_A,i);
+        i++;
+        
     }
 
 
-    mcp4822_initialize();
+    mcp4822_deinitialize();
 
     return(0);
 }
