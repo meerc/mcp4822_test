7927ccc1cbd966e8e2863c75bdee1dd881b79a1a
meerc <76517847+meerc@users.noreply.github.com>
2022-11-03 11:37:38 +0100

    Add files via upload

---
+++ test mcp4822/test_control.c
@@ -0,0 +1,28 @@
+#include "mcp4822.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main()
+{
+    bool chk=true;
+    uint16_t i=1000u;
+
+    mcp4822_initialize();
+
+
+    while(chk== true&& i<4095u)
+    {
+
+    chk=mcp4822_set_voltage(MCP_4822_CHANNEL_A,i);
+    }
+
+
+    mcp4822_initialize();
+
+    return(0);
+}
